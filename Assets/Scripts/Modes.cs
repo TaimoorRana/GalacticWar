0ea50b24734857e6a5b7a90b09c5336a67300c7f
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-09 16:31:56 -0400

    Added Sound and Main Menu

---
+++ Assets/Scripts/Modes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Modes : MonoBehaviour {
+	public static bool insanity = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadNormalMode(){
+		DontDestroyOnLoad (this);
+		Application.LoadLevel("Main");
+	}
+
+	public void loadInsanityMode(){
+		insanity = true;
+		DontDestroyOnLoad (this);
+		Application.LoadLevel("Main");
+	}
+}
