037418bc2a1655a92859fccf8978886c234253dc
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-09 12:43:41 -0400

    boss weak point mechanics working

--- Assets/Scripts/FinalBoss.cs
+++ Assets/Scripts/FinalBoss.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class FinalBoss : EnemyManager {
 	[SerializeField] GameObject bullet;
 	[SerializeField] float rotationSpeed;
+	[SerializeField] GameObject core;
 
 	public float speed;
 	float shootInterval;
 	float yBoundary,xBoundary, currentY, currentX;
 	bool goingDown = true, goingRight = true;
+	bool coreCreated = false;
 	[SerializeField] Transform[] bulletSpawnLocations;
+
+	int weakPoint1Hits = 0, weakPoint2Hits = 0, coreHits = 0;
 
 	// Use this for initialization
 	void Start () {
-		shootInterval = Random.Range(0.5f,1.0f);
+		shootInterval = Random.Range(3f,5f);
 		yBoundary = Camera.main.orthographicSize;
 		xBoundary =  (Screen.width / Screen.height) * yBoundary;
 		InvokeRepeating("LaunchProjectile", shootInterval, shootInterval);
@@ -24,6 +28,42 @@ public class FinalBoss : EnemyManager {
 		transform.Rotate (new Vector3(0,0,Time.deltaTime * rotationSpeed));
 		//MoveEnemiesVertically ();
 	}
+
+	public void HitWeakPoint( string WeakPointName){
+
+		switch (WeakPointName) {
+		case "WeakPoint1":
+			if (weakPoint1Hits < 2)
+				weakPoint1Hits++;
+			break;
+		case "WeakPoint2":
+			if (weakPoint2Hits < 2)
+				weakPoint2Hits++;
+			break;
+		case "Core(Clone)":
+ 			coreHits++;
+			if (coreHits > 1)
+				gameObject.SetActive (false);
+			break;
+		}
+
+		if (weakPoint1Hits + weakPoint2Hits >= 4 && !coreCreated) {
+			GameObject coreCopy = Instantiate (core) as GameObject;
+			coreCopy.transform.parent = transform;
+			coreCopy.transform.position = transform.position;
+			coreCreated = true;
+			Destroy(coreCopy,5f);
+			// reset core health after 5 seconds
+			Invoke ("resetHealth", 5f);
+		}	
+	}
+
+	void resetHealth(){
+		weakPoint1Hits = 0;
+		weakPoint1Hits = 0;
+		coreCreated = false;
+	}
+
 
 	void  LaunchProjectile(){
 		if (gameObject.activeSelf) {
@@ -37,6 +77,8 @@ public class FinalBoss : EnemyManager {
 		}
 
 	}
+
+
 
 	void MoveEnemiesVertically(){
 
@@ -53,5 +95,7 @@ public class FinalBoss : EnemyManager {
 		}
 		transform.position = new Vector3 (transform.position.x, currentY, 0);
 	}
+
+
 
 }
---
+++ Assets/WeakPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeakPoint : MonoBehaviour {
+	FinalBoss boss;
+	// Use this for initialization
+	void Start () {
+		boss = transform.parent.gameObject.GetComponent<FinalBoss> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Bullet") {
+			Debug.Log ("got hit");
+			boss.HitWeakPoint (gameObject.name);
+		}
+
+	}
+}
