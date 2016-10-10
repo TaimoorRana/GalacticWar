6450b2289b035683b657b4b95f8a54ca2625ef11
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-10 16:44:48 -0400

    done

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -108,16 +108,16 @@ public class PlayerManager : MonoBehaviour {
 		}
 
 		// Movements
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.W)  || Input.GetKey (KeyCode.UpArrow)  ) {
 			transform.position += new Vector3 (0, speed, 0);
 		}
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
 			transform.position += new Vector3 (0, -speed, 0);
 		}
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += new Vector3 (-speed, 0, 0);
 		}
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += new Vector3 (speed, 0, 0);
 		}
 
